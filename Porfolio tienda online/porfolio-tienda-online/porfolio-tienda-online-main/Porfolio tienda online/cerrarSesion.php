a150664ea4f5f8fb38b3439f6eb715989e1429c9
RMontesJ <148756305+RMontesJ@users.noreply.github.com>
2024-07-05 10:35:38 +0200

    Delete Porfolio tienda online/porfolio-tienda-online/porfolio-tienda-online-main directory

--- Porfolio tienda online/porfolio-tienda-online/porfolio-tienda-online-main/Porfolio tienda online/cerrarSesion.php
+++
@@ -1,5 +0,0 @@
-<?php
-session_start(); //Empezar sesión 
-session_destroy(); //Funcion para cerrar(eliminar) la sesion que estabamos usando
-header("Location: inicio_sesion.php");//Redirigir automaticamente a la página del login
-exit();//Finalizar el script
--- Porfolio tienda online/porfolio-tienda-online/porfolio-tienda-online-main/Porfolio tienda online/includes/busquedaUsuarios.php
+++
@@ -1,6 +0,0 @@
-<div class="busquedaUsuarios">
-    <form action="" method="post">
-    <input type="text" name="busqueda">
-    <input type="submit" name="enviar" value="Buscar">
-    </form>
-</div>
