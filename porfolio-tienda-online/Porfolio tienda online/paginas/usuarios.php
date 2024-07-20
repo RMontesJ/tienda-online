85ae3e06635f9b07720fc20db399babfdbff0dfe
RMontesJ <148756305+RMontesJ@users.noreply.github.com>
2024-07-20 11:51:35 +0200

    Add files via upload

---
+++ porfolio-tienda-online/Porfolio tienda online/paginas/usuarios.php
@@ -0,0 +1,69 @@
+
+<?php 
+
+error_reporting(0);
+
+$usuario = $_GET['id_user'];
+
+require_once "../BD/Datos.php";
+$datosUsuario = new Datos();
+
+// si la sesion no esta puesta o esta vacia
+if(!isset($usuario) || $usuario == ""){
+header("Location: ../paginas/inicio_sesion.php");
+}
+// coge el correo
+$correo = $datosUsuario->cogerCorreo($usuario);
+
+// si no es admin
+if (strpos($correo, "@admin.com") === false) {
+    header("Location: ../paginas/indexRegistrado.php?id_user=$usuario");
+    exit();
+}
+// si se le da al boton de buscar, coge el valor de la busqueda del include busquedaUsuarios.php
+// y lo pasa como argumento al metodo buscarUsuarios
+if(isset($_POST['enviar'])){
+    $valor = $_POST['busqueda'];
+}
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Usuarios</title>
+    <link rel="stylesheet" href="../css/usuarios.css?v=<?php echo time(); ?>">
+    <link rel="stylesheet" href="../css/nav.css">
+</head>
+<body>
+
+<div class="pagina">
+
+<?php include "../includes/navAdmin.php" ?>
+
+<h1>Usuarios</h1>
+
+<?php 
+// include con el campo de texto y boton de busqueda
+include "../includes/busquedaUsuarios.php"
+
+?>
+
+<div id="perfiles">
+<?php 
+
+ // ejecuta una sentencia a la base de datos que busca usuarios con el valor del campo del include
+ //busquedaUsuarios.php
+$imprimirUsuarios = $datosUsuario->buscarUsuarios($valor);
+
+
+?>
+</div>
+
+</div>
+    
+</body>
+</html>
+
---
+++ porfolio-tienda-online/README.md
@@ -0,0 +1,17 @@
+# porfolio-tienda-online
+Proyect porfolio as student of first year of DAM.
+install xampp and locate it in C:/xampp, open the program and start the services apache and mysql, click the button "admin" at MySQL.
+To start, create a user on phpMyAdmin with this credentials:
+name: Rafa,
+host: localhost,
+password: 1234.
+Go to SQL and execute the script of the database
+(the script of the database is in the proyect, in the folder BD, the file is called "BD_tienda_online.sql").
+
+Then, put the folder proyect in C:/xampp/htdocs. Go to google chrome
+and search "127.0.0.1" and open the proyect, opening "index.php" file located in paginas/index.php.
+
+Once you have open the proyect, you will have to register, and then, login.
+If your email contains "@admin.com", you will access to the administrator mode and you will can see, search, edit, create and delete products, and also see all users registered in the app.
+If your email doesnt contain "@admin.com", you have the simple user permission, what allows you only to see products and edit your user info.
+(Both, simple user and administrator can edit their own profiles information and delete their accounts).
