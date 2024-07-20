8fc8d03006aa9af053a6fa4031be7771e8d49bd9
[Rafael Montes] <rafaelmontesjimenez@gmail.com>
2024-07-20 11:59:20 +0200

    Deleted duplicate proyect

--- porfolio-tienda-online/Porfolio tienda online/paginas/cerrarSesion.php
+++
@@ -1,5 +0,0 @@
-<?php
-session_start(); //Empezar sesión 
-session_destroy(); //Funcion para cerrar(eliminar) la sesion que estabamos usando
-header("Location: ../paginas/inicio_sesion.php");//Redirigir automaticamente a la página del login
-exit();//Finalizar el script
