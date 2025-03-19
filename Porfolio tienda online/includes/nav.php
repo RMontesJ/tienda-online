cf124e75b8a906914c9c6ba1a668e64189f96dbf
[Rafael Montes] <rafaelmontesjimenez@gmail.com>
2025-03-19 09:07:45 +0100

    Nav con bootstrap

--- Porfolio tienda online/includes/nav.php
+++ Porfolio tienda online/includes/nav.php
@@ -8,18 +8,33 @@ $fotoPerfil = $datosUsuario->cogerFoto($usuario);
 
 ?>
 
-<header>
-    <img src="../img/logUnimatchIA.jpg" alt="">
-    <nav>
-        <ul>
-            <li><a href="../paginas/indexRegistrado.php?id_user=<?php echo $usuario; ?>">Inicio</a></li>
-            <li><a href="../paginas/ayuda.php?id_user=<?php echo $usuario; ?>">Ayuda</a></li>
-        </ul>
-    </nav>
-    <div class="profile-logout-box">
-    <a href="../paginas/notificaciones.php?id_user=<?php echo $usuario; ?>"><img src="../img/MaterialSymbolsNotifications.svg" alt="Carrito" style="width:40px;height:40px;"></a>
-    <a href="../paginas/carrito.php?id_user=<?php echo $usuario; ?>"><img src="../img/MdiCart.svg" alt="Carrito" style="width:40px;height:40px;"></a>
-    <a href="../paginas/perfil.php?id_user=<?php echo $usuario; ?>"><img src="../fotosUsuarios/<?php echo $fotoPerfil; ?>" alt="Foto usuario" style="width:40px;height:40px;"></a>
-    <a href="../paginas/cerrarSesion.php"><img src="../img/logout_icon.svg" alt="Logout" style="width:40px;height:40px;"></a>
+<nav class="navbar navbar-expand-lg navbar-dark bg-primary">
+    <div class="container">
+        <a class="navbar-brand" href="#">
+            <img src="../img/logUnimatchIA.jpg" alt="Logo" style="height: 50px;">
+        </a>
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarNav">
+            <ul class="navbar-nav me-auto">
+                <li class="nav-item"><a href="../paginas/indexRegistrado.php?id_user=<?php echo $usuario; ?>" class="nav-link">Inicio</a></li>
+                <li class="nav-item"><a href="../paginas/ayuda.php?id_user=<?php echo $usuario; ?>" class="nav-link">Ayuda</a></li>
+            </ul>
+            <div class="d-flex align-items-center">
+                <a href="../paginas/notificaciones.php?id_user=<?php echo $usuario; ?>" class="me-3">
+                    <img src="../img/MaterialSymbolsNotifications.svg" alt="Notificaciones" style="width:40px;height:40px;">
+                </a>
+                <a href="../paginas/carrito.php?id_user=<?php echo $usuario; ?>" class="me-3">
+                    <img src="../img/MdiCart.svg" alt="Carrito" style="width:40px;height:40px;">
+                </a>
+                <a href="../paginas/perfil.php?id_user=<?php echo $usuario; ?>" class="me-3">
+                    <img src="../fotosUsuarios/<?php echo $fotoPerfil; ?>" alt="Foto usuario" class="rounded-circle" style="width:40px;height:40px;">
+                </a>
+                <a href="../paginas/cerrarSesion.php">
+                    <img src="../img/logout_icon.svg" alt="Logout" style="width:40px;height:40px;">
+                </a>
+            </div>
+        </div>
     </div>
-</header>
+</nav>
--- Porfolio tienda online/includes/navAdmin.php
+++ Porfolio tienda online/includes/navAdmin.php
@@ -7,20 +7,38 @@ $datosUsuario = new Datos();
 $fotoPerfil = $datosUsuario->cogerFoto($usuario);
 
 ?>
+<nav class="navbar navbar-expand-lg navbar-dark bg-primary">
+        <div class="container">
+            <!-- Logo -->
+            <a class="navbar-brand" href="#">
+                <img src="../img/logUnimatchIA.jpg" alt="Logo" style="height: 50px;">
+            </a>
 
-<header>
-    <img src="../img/logUnimatchIA.jpg" alt="">
-    <nav>
-        <ul>
-            <li><a href="../paginas/indexRegistradoAdmin.php?id_user=<?php echo $usuario; ?>">Inicio</a></li>
-            <li><a href="../paginas/crearProducto.php?id_user=<?php echo $usuario; ?>">Subir producto</a></li>
-            <li><a href="../paginas/usuarios.php?id_user=<?php echo $usuario; ?>">Usuarios</a></li>
-            <li><a href="../paginas/ayudaAdmin.php?id_user=<?php echo $usuario; ?>">Ayuda</a></li>
-        </ul>
+            <!-- Botón para menú responsive -->
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+
+            <!-- Contenido del menú -->
+            <div class="collapse navbar-collapse" id="navbarNav">
+                <ul class="navbar-nav me-auto">
+                    <li class="nav-item"><a href="../paginas/indexRegistradoAdmin.php?id_user=<?php echo $usuario; ?>" class="nav-link">Inicio</a></li>
+                    <li class="nav-item"><a href="../paginas/crearProducto.php?id_user=<?php echo $usuario; ?>" class="nav-link">Subir producto</a></li>
+                    <li class="nav-item"><a href="../paginas/usuarios.php?id_user=<?php echo $usuario; ?>" class="nav-link">Usuarios</a></li>
+                    <li class="nav-item"><a href="../paginas/ayudaAdmin.php?id_user=<?php echo $usuario; ?>" class="nav-link">Ayuda</a></li>
+                </ul>
+                <!-- Iconos de perfil y logout -->
+                <div class="d-flex align-items-center">
+                    <a href="../paginas/notificaciones.php?id_user=<?php echo $usuario; ?>" class="me-3">
+                        <img src="../img/MaterialSymbolsNotifications.svg" alt="Notificaciones" style="width:40px;height:40px;">
+                    </a>
+                    <a href="../paginas/perfilAdmin.php?id_user=<?php echo $usuario; ?>" class="me-3">
+                        <img src="../fotosUsuarios/<?php echo $fotoPerfil; ?>" alt="Foto usuario" class="rounded-circle" style="width:40px;height:40px;">
+                    </a>
+                    <a href="../paginas/cerrarSesion.php">
+                        <img src="../img/logout_icon.svg" alt="Logout" style="width:40px;height:40px;">
+                    </a>
+                </div>
+            </div>
+        </div>
     </nav>
-    <div class="profile-logout-box">
-    <a href="../paginas/notificaciones.php?id_user=<?php echo $usuario; ?>"><img src="../img/MaterialSymbolsNotifications.svg" alt="Carrito" style="width:40px;height:40px;"></a>
-    <a href="../paginas/perfilAdmin.php?id_user=<?php echo $usuario; ?>"><img src="../fotosUsuarios/<?php echo $fotoPerfil; ?>" alt="Foto usuario" style="width:40px;height:40px;"></a>
-    <a href="../paginas/cerrarSesion.php"><img src="../img/logout_icon.svg" alt="Logout" style="width:40px;height:40px;"></a>
-    </div>
-</header>
